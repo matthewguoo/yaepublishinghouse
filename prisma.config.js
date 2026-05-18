8b1f772957ef7ebde782704b6e4aa079b61fef99
Matthew Guo <yaemikodayoo@gmail.com>
2026-05-18 01:59:38 -0700

    fix: remove prisma config, use constructor url only

--- prisma.config.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-
-module.exports = {
-  earlyAccess: true,
-  schema: path.join(__dirname, 'prisma', 'schema.prisma'),
-  migrate: {
-    url: process.env.DATABASE_URL || '',
-  },
-};
